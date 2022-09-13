f8da2b21fd1d814c6e813a487a6804c84b705459
Guilherme <guiggff@gmail.com>
2022-09-13 14:39:34 -0300

    feat: Adicionado cliente

--- src/endpoints/pix.ts
+++ src/endpoints/pix.ts
@@ -2,11 +2,25 @@ import fetch from 'node-fetch';
 import { v4 as uuidv4 } from 'uuid';
 import { RequestHandler } from 'express';
 
+interface ClienteProps {
+  name?: string;
+  email?: string;
+  phone?: string;
+  taxID?: string;
+}
+
 export const generatePix: RequestHandler = async (req, res) => {
   try {
-    const { valor } = req.body;
+    const { valor, cliente }: { valor: number; cliente: ClienteProps } =
+      req.body;
 
     const url = `${process.env.URL_OPENPIX}/charge`;
+
+    if (!cliente || !valor) {
+      return res.status(400).json({
+        error: 'Cliente ou Valor da transação está faltando.',
+      });
+    }
 
     const response = await fetch(url, {
       method: 'POST',
@@ -19,6 +33,13 @@ export const generatePix: RequestHandler = async (req, res) => {
         correlationID: uuidv4(),
         value: valor,
         expiresIn: 3600,
+        customer: {
+          name: cliente.name || '',
+          email: cliente.email || '',
+          phone: cliente.phone || '',
+          //taxID => CPF ou CNPJ
+          taxID: cliente.taxID || '',
+        },
       }),
     });
 
