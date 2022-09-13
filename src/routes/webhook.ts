bebd535e95df9f1617da58f4ac6805e94161e3d8
Guilherme <guiggff@gmail.com>
2022-09-13 16:12:26 -0300

    webhooks

--- src/routes/webhook.ts
+++ src/routes/webhook.ts
@@ -1,11 +1,8 @@
 import { Router } from 'express';
+import { webhook } from '../endpoints/webhook';
 
 const router = Router();
 
-router.post('/webhook', (req, res) => {
-  console.log(req.body);
-
-  res.send(200);
-});
+router.post('/webhook', webhook);
 
 export default router;
